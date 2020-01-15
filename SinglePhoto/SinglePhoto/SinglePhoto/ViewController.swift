3589617b3ce8a3b69490df729c074625bbb5dfe7
student <student@dc-98.3500.dhcp.asu.edu>
2020-01-15 13:22:52 -0700

    first app

---
+++ SinglePhoto/SinglePhoto/SinglePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SinglePhoto
+//
+//  Created by student on 1/15/20.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
