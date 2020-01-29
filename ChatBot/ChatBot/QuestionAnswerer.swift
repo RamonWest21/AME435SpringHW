c78604502083a966594bdf6ab45cb919beac2a94
student <student@dc-110.3500.dhcp.asu.edu>
2020-01-29 13:15:45 -0700

    chatbot

---
+++ ChatBot/ChatBot/QuestionAnswerer.swift
@@ -0,0 +1,52 @@
+struct QuestionAnswerer {
+    /// Creates a String in response to another String.
+    func responseTo(question: String) -> String {
+        let lowerQuestion = question.lowercased()
+        
+        if lowerQuestion == "where are the cookies?" {
+            return "In the cookie jar!"
+        }
+        else if lowerQuestion == "what is your name?" {
+            return "My name is HAL, I am an Heuristically Programmed ALgorithmic Computer."
+        }
+        else if lowerQuestion.hasPrefix("can you") {
+            return "🙅‍♂️ I'm afraid I can't do that."
+        }
+        else if lowerQuestion.hasSuffix("from") || lowerQuestion.hasSuffix("from?") {
+            return "From the depths of Jabba's palace."
+        }
+        else if lowerQuestion.hasPrefix("where are") || lowerQuestion.hasPrefix("where is") {
+            return "Right around the corner."
+        }
+        else if lowerQuestion.hasPrefix("what") {
+            return "That!"
+        }
+        else if lowerQuestion.hasPrefix("hello") {
+            return "Howdy!"
+        }
+        else {
+            
+            let defaultNumber = question.count % 6
+            
+            if defaultNumber == 0 {
+                return "🤷‍♂️ That really depends."
+            }
+            else if defaultNumber == 1 {
+                return "🧞‍♂️ Ask me again tomorrow. "
+            }
+            else if defaultNumber == 2 {
+                return "😆 I wish I could tell you."
+            }
+            else if defaultNumber == 3 {
+                return "💁‍♂️ I'm not sure I understand."
+            }
+            else if defaultNumber == 4 {
+                return "Are you sure you are spelling that correctly?"
+            }
+            else {
+                return "Go bother Alexa with that."
+            }
+            
+        }
+    }
+}
