1c79ea214db29194f55c5555181486779bc69ec2
student <student@dc-112.3500.dhcp.asu.edu>
2020-02-10 10:29:35 -0700

    CommonInputControls

--- CommonInputControls/CommonInputControls/ViewController.swift
+++ CommonInputControls/CommonInputControls/ViewController.swift
@@ -12,12 +12,28 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var toggle: UISwitch!
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
     }
 
+    @IBAction func setTextButtonTapped(_ sender: UIButton) {
+        textLabel.text = textField.text
+    }
+    @IBAction func clearTextButtonTapped(_ sender: UIButton) {
+        textLabel.text = ""
+        textField.text = ""
+        
+    }
+    @IBAction func respondToTapGesture(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: view)
+        print(location)
+    }
     @IBAction func buttonTapped(_ sender: UIButton) {
         print("Button was tapped")
         if toggle.isOn {
